470e43fd95b657f214dd039d033e92424401f354
thaparahul <connectthapa84@gmail.com>
2019-06-26 23:25:09 -0700

    modified the DOM's file to create a shopping list

--- DOM/Exercise/script.js
+++ DOM/Exercise/script.js
@@ -1,6 +1,21 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var deleteBtns = document.getElementsByClassName("delete");
+
+
+for (var i=0; i<deleteBtns.length; i++){
+	deleteBtns[i].addEventListener("click", removeParent, false);
+}
+
+function removeParent(event){
+	// event.target.removeEventListener("click", removeParent, false);
+	event.target.parentNode.remove();
+}
+
+ul.onclick = function(event){
+	event.target.classList.toggle("done");
+}
 
 function inputLength() {
 	return input.value.length;
@@ -8,10 +23,24 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
+	var btn = document.createElement("button");
+
+	btn.appendChild(document.createTextNode("Delete"));
+	btn.onclick = removeParent;
+
 	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(btn);
 	ul.appendChild(li);
 	input.value = "";
 }
+
+// function removeItem(event) {
+// 	event.target.parentNode.remove();
+// }
+
+// function deleteButton(){
+
+// }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
@@ -25,6 +54,12 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function toggleListElement(event) {
+	event.classList.toggle("done");
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", toggleListElement);
